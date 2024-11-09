245bfc33cbf84c9e4dcdcc83d18b9c9c1357ce43
Kokko00 <163394398+Kokko00@users.noreply.github.com>
2024-11-09 11:30:36 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_template (
+module tt_um_test_8 (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
